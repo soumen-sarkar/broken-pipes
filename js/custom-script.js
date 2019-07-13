26806decd1418fc5f689e82cd7547fb6a383e628
Soumen Sarkar <soumen.rim@gmail.com>
2019-07-13 22:39:01 +0530

    First commit

---
+++ js/custom-script.js
@@ -0,0 +1,13 @@
+jQuery(window).load(function(){
+	//Background Image carousel
+	$('.testimonialSlider').carousel({
+		interval: 15000
+	})	 
+	//Script of equal height
+	$(function(cash) {
+		$('.equal').responsiveEqualHeightGrid(); 
+	});
+	$(function(cash) {
+		$('.equalTwo').responsiveEqualHeightGrid(); 
+	});	
+});
